607898f1c0fcb96743b6364ac9c803de9564df30
Soichiro-Yoshimura <yoshimura@soichiro.org>
2019-04-22 15:55:33 +0900

    とりあえずブロックが崩れるところまで

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ phina.globalize();
 
 const BALL_RADIUS = 10;
 const BALL_INIT_POS_OFFSET = BALL_RADIUS * 10;
-const BALL_SPEED = 20;
+const BALL_SPEED = 50;
+const BLOCK_WIDTH = 52;
+const BLOCK_HEIGHT = 20;
+const MAX_PER_LINE_X = 8;
+const MAX_PER_LINE_Y = 10;
+const BLOCK_NUM = MAX_PER_LINE_X * MAX_PER_LINE_Y;
+const BOARD_PADDING_X = 100;
+const BOARD_PADDING_Y = 100;
 
 // MainScene クラスを定義
 phina.define('MainScene', {
@@ -18,6 +25,25 @@ phina.define('MainScene', {
     this.ball = Ball().addChildTo(this);
     this.ball.x = this.gridX.center();
     this.ball.y = this.gridY.width - BALL_INIT_POS_OFFSET;
+
+    // ブロックを設置
+    this.group = DisplayElement().addChildTo(this);
+    const gridX = Grid(this.gridX.width - (BOARD_PADDING_X * 2), MAX_PER_LINE_X);
+    const gridY = Grid((this.gridY.width / 1.3) - (BOARD_PADDING_Y * 2), MAX_PER_LINE_Y);
+    (BLOCK_NUM).times(function(i) {
+      const xIndex = i % MAX_PER_LINE_X;
+      const yIndex = Math.floor(i / MAX_PER_LINE_X);
+      const block = Block().addChildTo(this.group);
+      // console.log('i:' + i);
+      // console.log('xIndex:' + xIndex);
+      // console.log('yIndex:' + yIndex);
+      // console.log('gridX.span(xIndex):' + gridX.span(xIndex));
+      // console.log('gridY.span(yIndex):' + gridY.span(yIndex));
+      // console.log('--------------------');
+      block.x = gridX.span(xIndex) + BOARD_PADDING_X + BLOCK_WIDTH / 2;
+      block.y = gridY.span(yIndex) + BOARD_PADDING_Y + BLOCK_HEIGHT / 2;
+    }, this);
+    
   },
 
   update: function(app) {
@@ -28,6 +54,8 @@ phina.define('MainScene', {
       this.ball.move();
       this.checkHit();
     }, this);
+
+  
   },
 
   checkHit: function() {
@@ -50,6 +78,34 @@ phina.define('MainScene', {
       ball.bottom = this.gridY.width
       ball.reflectY();
     }
+
+      // ボールをの当たり判定をして取り除く
+      this.group.children.some(function(block) {
+        if (ball.hitTestElement(block)) {
+          var dq = Vector2.sub(ball, block);
+  
+          if (Math.abs(dq.x) < Math.abs(dq.y)) {
+            ball.reflectY();
+            if (dq.y >= 0) {
+              ball.top = block.bottom;
+            }
+            else {
+              ball.bottom = block.top;
+            }
+          }
+          else {
+            ball.reflectX();
+            if (dq.x >= 0) {
+              ball.left = block.right;
+            }
+            else {
+              ball.right = block.left;
+            }
+          }
+          block.remove();
+          return true;
+        }
+      }, this);
   },
 });
 
@@ -88,6 +144,24 @@ phina.define('Ball', {
   },
 });
 
+
+/**
+ * ブロックを定義
+ */
+phina.define('Block', {
+  superClass: 'RectangleShape',
+
+  init: function() {
+    this.superInit({
+      width: BLOCK_WIDTH,
+      height: BLOCK_HEIGHT,
+      fill: '#70C1B3',
+      stroke: '#50514F',
+      strokeWidth: 2,
+      cornerRadius: 0,
+    });
+  },
+});
 
 // メイン処理
 phina.main(function() {
